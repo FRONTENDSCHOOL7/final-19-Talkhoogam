16d164e73059efa06bcc03a37f6785f704215917
coo1river <longpaca@gmail.com>
2023-10-31 10:27:44 +0900

    ✨ feat:  이미지 api 코드 추가, router 추가

--- src/router/AppRouters.jsx
+++ src/router/AppRouters.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
-import Welcome from '../pages/Welcome';
-import Login from '../pages/Login';
-import Join from '../pages/Join';
-import Gathering from '../pages/Gathering';
-import Header from '../components/header/BasicHeader';
-import Profile from '../pages/profile/Profile';
-import NotFound from '../pages/NotFound';
-import SetProfile from '../pages/profile/SetProfile';
-import ProductAdd from '../pages/product/ProductAdd';
-import ProductList from '../pages/product/ProductList';
-import ProductDetail from '../pages/product/ProductDetail';
-import ProductModify from '../pages/product/ProductModify';
-import PostUpload from '../pages/post/PostUpload';
-import Followings from '../pages/profile/Followings';
-import Followers from '../pages/profile/Followers';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Welcome from "../pages/Welcome";
+import Login from "../pages/Login";
+import Join from "../pages/Join";
+import Gathering from "../pages/Gathering";
+import Header from "../components/header/BasicHeader";
+import Profile from "../pages/profile/Profile";
+import NotFound from "../pages/NotFound";
+import SetProfile from "../pages/profile/SetProfile";
+import ProductAdd from "../pages/product/ProductAdd";
+import ProductList from "../pages/product/ProductList";
+import ProductDetail from "../pages/product/ProductDetail";
+import ProductModify from "../pages/product/ProductModify";
+import PostUpload from "../pages/post/PostUpload";
+import Followings from "../pages/profile/Followings";
+import Followers from "../pages/profile/Followers";
+import EditProfile from "../pages/profile/EditProfile";
 
 export default function Routers() {
   return (
@@ -28,6 +29,7 @@ export default function Routers() {
       <Route path="/gathering" element={<Gathering />} />
       <Route path="/header" element={<Header />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/editprofile" element={<EditProfileProfile />} />
       <Route path="/profile/followings" element={<Followings />} />
       <Route path="/profile/followers" element={<Followers />} />
       <Route path="/productAdd" element={<ProductAdd />} />

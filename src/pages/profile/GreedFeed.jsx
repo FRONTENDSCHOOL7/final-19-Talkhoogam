0abf9b3c818602973272801eb237bce30ad2030b
yebin Park <yebomholmes>
2023-11-02 09:47:30 +0900

    � fix: accountname 오류 수정 완료, 더보기 하트 기능 추가

--- src/pages/profile/GreedFeed.jsx
+++ src/pages/profile/GreedFeed.jsx
@@ -4,9 +4,11 @@ import Modal from "react-modal";
 import styled from "styled-components";
 import ImgLayes from "../../assets/icons/icon-img-layers.svg";
 import MyFeedListAPI from "../../api/post/MyFeedListAPI.jsx";
+import Empty from '../../components/empty/Empty';
+import LogoImg from "../../assets/images/Logo.png"
 
 export default function LayerFeed(accountname){
-
+    const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
     const {getMyFeedListAPI} = MyFeedListAPI(accountname);
     const [myFeedData, setMyFeedData] = useState(() => {});
@@ -27,34 +29,40 @@ export default function LayerFeed(accountname){
     fetchData();
     }, []);
     console.log('myFeedData : ', myFeedData)
-    console.log(accountname.accountname)
+    //console.log(accountname.accountname)
 
-    //썸네일 클릭
-    const onClickThumbnail = () => {
-        alert("구현 X")
-    }
     return(
         <Layer>
             {myFeedData ? (
             myFeedData.post.map((item, index) => (
                 <div className="feedlayer" key={index}>
-                <button className="content" onClick={onClickThumbnail}>
+                <button className="content" >
                     {" "}
                     {/*피드가 추가될때마다 content div 추가*/}
+                    <MoreButton onClick={() => navigate(`/post/detail/${item.id}`)}>
                     <img src={item.image} alt="예시 사진" className="bookImg"></img>
-                    <img src={ImgLayes} alt="이미지 2장 이상일 경우 나타나는 아이콘" className="bookMultiple"></img>
+                    {item.image.length > 1 ? (<img src={ImgLayes} alt="이미지 2장 이상일 경우 나타나는 아이콘" className="bookMultiple"></img>) : (<p className='a11y-hidden'>1장이에용</p>)}
+                    </MoreButton>
                 </button>
                 </div>
             ))) : (
-                <p>오류</p>
+                <>
+                <h1 className='a11y-hidden'>팔로우가 존재하지 않습니다.</h1>
+                <Empty image={LogoImg} alt={"404페이지"} >
+                  유저를 검색해 팔로우 해보세요!
+                </Empty>
+              </>
             )}
             </Layer>
     )
 }
+const MoreButton = styled.div`
+    cursor: pointer;
+    border: none;
+`
 const Layer = styled.div`
     display: grid; // 재웅 그리드로 바꾸고 도망갑니다
     grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(3, 1fr);
     gap: 12px;
     margin: 16px;
     margin-bottom: 80px;
--- src/pages/profile/ListFeed.jsx
+++ src/pages/profile/ListFeed.jsx
@@ -4,14 +4,24 @@ import Modal from "react-modal";
 import styled from "styled-components";
 import ImgVertical from "../../assets/icons/s-icon-more-vertical.svg";
 import IconHeart from "../../assets/icons/heart.svg";
+import IconHeartActive from "../../assets/icons/heart-avtive.svg";
 import IconMessage from "../../assets/icons/icon-message-circle.svg";
 import MyFeedListAPI from "../../api/post/MyFeedListAPI.jsx";
-import accountname from "../../recoil/accountname";
+import accountName from "../../recoil/accountname";
+import Empty from '../../components/empty/Empty';
+import LogoImg from "../../assets/images/Logo.png"
+import CommonModal from '../../components/modal/CommonModal';
+import { useRecoilValue } from "recoil";
 
 export default function ListFeed(accountname) {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const { getMyFeedListAPI } = MyFeedListAPI(accountname);
   const [myFeedData, setMyFeedData] = useState(() => {});
+  const [modalOpen, setModalOpen ] = useState(false);
+  const [isMine, setIsMine] = useState(false);
+  const loginName = useRecoilValue(accountName);
+  const [postId, setPostId] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +37,28 @@ export default function ListFeed(accountname) {
     };
     fetchData();
   }, []);
-  console.log("myFeedData : ", myFeedData);
+  //console.log("myFeedData : ", myFeedData);
+//더보기 버튼
+  const showModal = (name, id) => {
+    modalOpen ? setModalOpen(false) : setModalOpen(true);
+    setPostId(id);
+    if(name === loginName){
+      setIsMine(true)  
+    } else {
+      setIsMine(false)
+    }
+}
+
+//하트 색칠하기
+const [iconColor, setIconColor] = useState(IconHeart);
+
+const colorChangeHandler = () => {
+    if(iconColor === IconHeart){
+        setIconColor(IconHeartActive);
+    }else{
+        setIconColor(IconHeart);
+    }
+}
 
   return (
     <UlStyled>
@@ -42,6 +73,7 @@ export default function ListFeed(accountname) {
             <div className="feedlist">
               <div>
                 <img
+                    onClick={() => {showModal(item.author.accountname, item.id)}}
                   className="list-vertical"
                   src={ImgVertical}
                   alt="vertical 탭"
@@ -49,18 +81,19 @@ export default function ListFeed(accountname) {
                 <p className="list-name">{item.author.username}</p>
                 <p className="list-id">{item.author.accountname}</p>
               </div>
-              <img
-                onClick={myFeedData}
-                className="list-img"
-                src={item.image}
-                alt="피드 사진"
-              />
+              <MoreButton onClick={() => navigate(`/post/detail/${item.id}`)}>
+                <img
+                    className="list-img"
+                    src={item.image}
+                    alt="피드 사진"
+                />
+              </MoreButton>
               <p className="list-text">{item.content}</p>
               <div className="list-icon">
-                <img src={IconHeart} alt="좋아요" />
+                <img onClick={colorChangeHandler} src={iconColor} alt="좋아요"/>
                 <p>{item.heartCount}</p>
                 <img src={IconMessage} alt="댓글" />
-                <p>{item.comments}</p>
+                <p>{item.commentCount}</p>
               </div>
               <div className="list-date">
                 <p>{item.createdAt}</p>
@@ -69,11 +102,28 @@ export default function ListFeed(accountname) {
           </List>
         ))
       ) : (
-        <p>오류</p>
+        <>
+        <h1 className='a11y-hidden'>팔로우가 존재하지 않습니다.</h1>
+        <Empty image={LogoImg} alt={"404페이지"} >
+          유저를 검색해 팔로우 해보세요!
+        </Empty>
+      </>
       )}
+        { modalOpen && 
+        <CommonModal 
+        isMine={isMine}  
+        id={postId}
+        setModalOpen={setModalOpen}
+        isLocation={`post`}
+        ></CommonModal>
+      }
     </UlStyled>
   );
 }
+const MoreButton = styled.div`
+    cursor: pointer;
+    border: none;
+`
 const List = styled.li`
   display: flex;
   justify-content: space-between;
@@ -117,6 +167,9 @@ const List = styled.li`
   .list-icon img {
     width: 20px;
     height: 20px;
+  }
+  .list-icon img:hover {
+      cursor: pointer;
   }
   .list-icon p {
     margin: 4px 6px;

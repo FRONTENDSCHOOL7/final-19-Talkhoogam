92ffe5b3011c4c653271a5902a0e65f5eb2d247b
nonelijah <elia341947@gmail.com>
2023-11-03 11:06:56 +0900

    🔨 refactor: 불필요한 코드 삭제 #108

--- src/components/profile/BtnUnFollowing.jsx
+++ src/components/profile/BtnUnFollowing.jsx
@@ -1,46 +1,18 @@
 import React, { useState } from 'react';
 import { BtnFollow } from '../../styles/FollowStyled';
-import FollowAPI from '../../api/profile/FollowAPI';
-import UnFollowAPI from '../../api/profile/UnFollowAPI';
 
-export default function BtnUnFollowing({ item }) {
+export default function BtnUnFollowing({ handleFollow, handleUnFollow }) {
   const [checkedActive, setCheckedActive] = useState(true);
-
-  // 팔로잉
-  const { followUser } = FollowAPI(item.accountname);
-  const handleFollow = async () => {
-    try {
-      const res = await followUser();
-      console.log(res);
-      console.log('팔로우 하는데 성공하였습니다.');
-    } catch (error) {
-      console.error('팔로우 하는데 실패하였습니다.', error);
-    }
-  };
-
-  // 언팔로잉
-  const { unFollowUser } = UnFollowAPI(item.accountname);
-  const handleUnFollow = async () => {
-    try {
-      const res = await unFollowUser();
-      console.log(res);
-      console.log('언팔로우 하는데 성공하였습니다.');
-    } catch (error) {
-      console.error('언팔로우 하는데 실패하였습니다.', error);
-    }
-  };
 
   // 언팔로잉버튼 클릭시
   function unFollowClicked() {
     setCheckedActive(false);
-    // unFollowFunc();
     handleUnFollow();
   }
 
   // 취소버튼 클릭시
   function cancelClicked() {
     setCheckedActive(true);
-    // followingFunc();
     handleFollow();
   }
 
--- src/components/profile/FollowItems.jsx
+++ src/components/profile/FollowItems.jsx
@@ -5,8 +5,34 @@ import { useRecoilValue } from 'recoil';
 import accountname from '../../recoil/accountname';
 import BtnUnFollowing from './BtnUnFollowing';
 import BtnFollowing from './BtnFollowing';
+import FollowAPI from '../../api/profile/FollowAPI';
+import UnFollowAPI from '../../api/profile/UnFollowAPI';
 
 export default function FollowItems({ item }) {
+  // 팔로잉
+  const { followUser } = FollowAPI(item.accountname);
+  const handleFollow = async () => {
+    try {
+      const res = await followUser();
+      console.log(res);
+      console.log('팔로우 하는데 성공하였습니다.');
+    } catch (error) {
+      console.error('팔로우 하는데 실패하였습니다.', error);
+    }
+  };
+
+  // 언팔로잉
+  const { unFollowUser } = UnFollowAPI(item.accountname);
+  const handleUnFollow = async () => {
+    try {
+      const res = await unFollowUser();
+      console.log(res);
+      console.log('언팔로우 하는데 성공하였습니다.');
+    } catch (error) {
+      console.error('언팔로우 하는데 실패하였습니다.', error);
+    }
+  };
+
   // 클릭한 유저 페이지로 이동
   const navigate = useNavigate();
   const navigateUserProfile = () => {
@@ -26,9 +52,17 @@ export default function FollowItems({ item }) {
           </div>
         </button>
         {item.accountname !== loginAccount && item.isfollow ? (
-          <BtnUnFollowing item={item} />
+          <BtnUnFollowing
+            item={item}
+            handleFollow={handleFollow}
+            handleUnFollow={handleUnFollow}
+          />
         ) : (
-          <BtnFollowing item={item} />
+          <BtnFollowing
+            item={item}
+            handleFollow={handleFollow}
+            handleUnFollow={handleUnFollow}
+          />
         )}
       </FollowLiStyle>
     </>

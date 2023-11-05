ee2177386722b05d943ea421735ccda901a54cf2
nonelijah <elia341947@gmail.com>
2023-11-05 16:38:09 +0900

    🎨 style: 헤더 pageName 및 더보기 이미지 색상 변경. #57

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   BackBtn,
   HeaderMain,
   LogoImg,
   MenuBtn,
   SearchBtn,
-} from "../../styles/HeaderStyled";
-import MoreList from "./MoreList";
-import iconArrow from "../../assets/icons/icon-arrow-left.svg";
-import iconMore from "../../assets/icons/s-icon-more-vertical.svg";
-import iconSearch from "../../assets/icons/icon-search.svg";
-import logoImg from "../../assets/images/Logo.png";
-import { useLocation, useNavigate } from "react-router-dom";
+} from '../../styles/HeaderStyled';
+import MoreList from './MoreList';
+import iconArrow from '../../assets/icons/icon-arrow-left.svg';
+import iconMore from '../../assets/icons/s-icon-more-vertical.svg';
+import iconSearch from '../../assets/icons/icon-search.svg';
+import logoImg from '../../assets/images/Logo.png';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function MainHeader({ pageName }) {
   const [moreOpen, setMoreOpen] = useState(false);
@@ -32,20 +32,21 @@ export default function MainHeader({ pageName }) {
   return (
     <>
       <HeaderMain>
-        {location.pathname === "/home" ||
-        location.pathname === "/gathering" ||
-        location.pathname === "/chat" ? (
-          <div style={{ width: "24px" }} />
+        {location.pathname === '/home' ||
+        location.pathname === '/gathering' ||
+        location.pathname === '/chat' ? (
+          <div style={{ width: '24px' }} />
         ) : (
           <BackBtn className="btn-goBack" onClick={goBack}>
             <img src={iconArrow} alt="뒤로가기 버튼" />
           </BackBtn>
         )}
+        <h1 className="a11y-hidden">톡후감</h1>
         {pageName && <h2>{pageName}</h2>}
-        {location.pathname === "/home" ? (
+        {location.pathname === '/home' ? (
           <LogoImg src={logoImg} alt="로고" />
         ) : null}
-        {location.pathname === "/home" ? (
+        {location.pathname === '/home' ? (
           <SearchBtn
             onClick={() => {
               navigate(`/search`);
